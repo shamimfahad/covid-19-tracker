94dd67b9d8000b9d71a3a380f3570b8d9783ae15
shamimfahad <shamimfahadshuvo@gmail.com>
2020-04-24 19:23:02 +0600

    Updates

---
+++ precache-manifest.5a5e1286b9026198b31eaafcc69c81b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa62370187fd0155678dc759d77b2d0",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "7a5066110148e74a9b1e",
+    "url": "/covid19-tracker/static/css/main.91931f7a.chunk.css"
+  },
+  {
+    "revision": "b21d6eb23ffc5bec9106",
+    "url": "/covid19-tracker/static/js/2.41bbc012.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19-tracker/static/js/2.41bbc012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a5066110148e74a9b1e",
+    "url": "/covid19-tracker/static/js/main.9afc7997.chunk.js"
+  },
+  {
+    "revision": "eb5ff0e51105f2f9eece",
+    "url": "/covid19-tracker/static/js/runtime-main.782e1f46.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
